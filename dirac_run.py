27fa600e8d984ede856f0a90f3b172df12a91361
arthurbarnard <arthurbarnard@gmail.com>
2016-10-21 14:26:07 -0700

    Add files via upload

---
+++ dirac_run.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import pickle
+from matplotlib.path import Path
+from dirac_sheet import dirac_sheet
+
+
+def main():
+	fig = plt.figure(num=None, figsize=(11, 6), dpi=96, facecolor='w', edgecolor='k')
+
+	#imput parameters for Dirac_sheet
+	dx = .25
+	dt = 0.1
+	Ngrid = 1802
+	X_offset=112.5
+	Nsteps=2200
+	z_lim=2
+	filename='Dirac_run_out'
+
+	plt.ion()
+	plt.clf()
+
+	tic = time.time()
+	
+	myDirac=dirac_sheet(0,901,dt,dx,X_offset,0)
+	myDirac.set_p(.1,np.pi*0)
+	
+	x, y = myDirac.get_pos_mat()
+	
+
+	NoPropMat = np.zeros(x.shape,dtype=np.uint8)
+	
+	#define part of injector shape
+	poly_verts = np.array([[0,37],[70,12.5],[70,1001],[0,1001],[0,37]])
+	
+	NoPropMat[inPolygon(poly_verts,x,y)]=1
+	
+	# mirror across x-axis and make CCW
+	poly_verts[:,1]*=-1
+	poly_verts[:,1]-=.125
+	poly_verts[:,:]=poly_verts[::-1,:]
+	NoPropMat[inPolygon(poly_verts,x,y)]=1
+	
+	#define second aperture
+	NoPropMat[((x<140.26)&(x>140)&(y>12.5))]=1
+	NoPropMat[((x<140.26)&(x>140)&(y<-12.5))]=1
+	
+	#Define absorption matrix
+	AbsMat = np.zeros(x.shape)
+	AbsMat[x>205]=.99
+	AbsMat[(x>70)&(x<141)&(y>40)]=.99
+	AbsMat[(x>70)&(x<141)&(y<-40)]=.99
+	
+	DriveMat=np.zeros(x.shape)
+	DriveMat[(x>0)&(x<1)&(y>-36)&(y<36)]=1
+	
+	myDirac.set_No_prop_mat(NoPropMat)
+	myDirac.set_Absorb_mat(AbsMat)
+	myDirac.set_Drive_mat(DriveMat)
+	
+	
+	for i in xrange(0,Nsteps):
+		#timestep
+		myDirac.time_step()
+		toc = time.time()
+		print i, toc-tic
+		
+		
+		if np.mod(i,10)==0:
+			#print colormap every 10 step
+			plt.clf()
+			
+			
+			#z limits
+			z_min, z_max = -z_lim, z_lim #np.min(np.real(myDirac.u1)), np.max(np.real(myDirac.u1))
+
+
+			
+			plt.subplot(1,2, 1)
+			plt.imshow(np.real(myDirac.u1), cmap='RdBu', vmin=z_min, vmax=z_max,
+					   extent=[x.min(), x.max(), y.min(), y.max()],
+					   interpolation='nearest', origin='lower')
+			plt.title('u1')
+			ax = plt.gca()
+			ax.set_xlim([0,200])
+			plt.colorbar()
+			plt.subplot(1,2,2)
+			plt.imshow(np.real(myDirac.v1), cmap='RdBu', vmin=z_min, vmax=z_max,
+					   extent=[x.min(), x.max(), y.min(), y.max()],
+					   interpolation='nearest', origin='lower')
+			plt.title('v1')
+			ax = plt.gca()
+			ax.set_xlim([0,200])
+			plt.colorbar()
+			
+			fig.canvas.draw()
+			
+	
+	#save key variables from simulation for later analysis
+	np.savez_compressed(filename+'.npz', u1=myDirac.u1,u2=myDirac.u2,v1=myDirac.v1,v2=myDirac.v2,Ngrid=myDirac.Ngrid,t=myDirac.t,D_x=myDirac.D_x)
+	plt.ioff()
+	plt.draw()
+	plt.show()
+		
+		
+def inPolygon(poly_verts,x,y):
+	#outputs logical array for which x,y are inside poly_verts
+	ss=x.shape
+	
+	x1, y1 = x.flatten(), y.flatten()
+
+	points = np.vstack((x1,y1)).T
+
+	path = Path(poly_verts)
+	
+	grid = path.contains_points(points, radius=.01)
+	grid = grid.reshape(ss)
+	
+	return grid
+		
+if __name__ == '__main__':
+	main()
---
+++ dirac_sheet.py
@@ -0,0 +1,253 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from matplotlib.path import Path
+pi=np.pi
+
+# "dirac_sheet.py" is an object class that defines a 2D discretized sheet for calculating solutions to the 
+# time-domain solutions of the Dirac equation. It implements a staggered time and staggered space discretization
+# approach outlined in Journal of Computational Physics 289(2015) 169-180. The discretized space is constructed of 
+# checkerboard "u" and "v" lattice. The checkerboard is formed by explicitly defining a "u1","u2","v1", and "v2"
+# grid that forms a unit cell on a square grid. Within a unit cell, "u1" is the upper-right, "u2" is the lower 
+# left, "v1" is the upper left, and #v2 is the lower right.
+#
+# The default sheet is a square of NgridxNgrid unit cells with periodic boundary conditions. Further boundary 
+# conditions can be set by three matrices: No_prop_mat, Absorb_mat, and Drive_mat. No_prop_mat defines regions
+# over which no propagation occurs (similar to defining the edge of the 2D material). Absorb_mat defines regions
+# where the wave is "lossy". This is primarily intended to introduce absorptive boundary conditions. Drive_mat
+# defines a region from which the electron wave is sourced.
+#
+# The properties of the sourced electrons are defined by the set_p method. p=2*pi/lambda is the injected electron
+# wave-vector magnitude, and theta is the wave-vector direction. The code only computes plane-wave injection with
+# the expectation that the user can sum several solutions to localize the injection.
+# 
+# For boundary conditions, the u-lattice is set to zero in the "No_prop" region, and the v-lattice is free. The drive
+# wave is implemented only on the v-lattice. In this sense, the "u" lattice is the master, and the "v" the slave. 
+class dirac_sheet:
+
+	def __init__(self,m, Ngrid, D_t, D_x, X_offset,Y_offset):
+	
+		self.t=0.0 			   	#time
+		self.Drive_coupling=0.3 #coupling strength of Drive plane-wave
+		self.Abs_rate=0.99		#exponential decay rate in absorptive regions
+		self.m=m				#effective mass; in graphene=0
+		self.Ngrid=Ngrid		#number of sub-lattice points: total (2Ngrid)x(2Ngrid) points
+		self.D_t=D_t			#discrete time step
+		self.D_x=D_x			#spatial discretization
+		self.D_y=D_x
+		self.X_offset=X_offset	
+		self.Y_offset=Y_offset
+		self.p0=2*pi/100.0		#Drive wave-vector magnitude
+		self.theta=0			#Drive wave-vector direction
+		self.px=self.p0*np.cos(self.theta)
+		self.py=self.p0*np.sin(self.theta)
+		self.y, self.x = np.mgrid[slice((0-round(Ngrid/2)),(Ngrid-round(Ngrid/2)),1),
+								slice((0-round(Ngrid/2)),(Ngrid-round(Ngrid/2)),1)]
+								
+		#define the X,Y coordinate matrix for each sublattice point
+		self.x, self.y = self.x*self.D_x+self.X_offset, self.y*self.D_y+self.Y_offset
+		self.xhalf, self.yhalf = self.x-self.D_x/2.0, self.y-self.D_y/2.0
+		
+		self.No_prop_mat=np.zeros((self.x.shape[0]*2,self.x.shape[1]*2))
+		self.Absorb_mat=np.zeros((self.x.shape[0]*2,self.x.shape[1]*2))
+		self.Drive_mat=np.zeros((self.x.shape[0]*2,self.x.shape[1]*2))
+		
+		#legacy, should be equivalent to theta
+		self.phi_p=np.arctan2(self.py,self.px)
+		
+		#out and out2 are temporary placeholders used in the wave-equation calculation
+		#this enables multiple manipulations of the wave matrix without overwriting
+		self.out=np.zeros(self.x.shape,dtype=np.complex64)
+		self.out2=np.zeros(self.x.shape,dtype=np.complex64)
+		#defining the u1,u2,v1,v2 sublattices
+		self.u1=np.zeros(self.x.shape,dtype=np.complex64)
+		self.u2=np.zeros(self.x.shape,dtype=np.complex64)
+		self.v1=np.zeros(self.x.shape,dtype=np.complex64)
+		self.v2=np.zeros(self.x.shape,dtype=np.complex64)
+		
+		#Drive plane wave
+		self.u10=np.exp(1j*(self.px*self.x+self.py*self.y))
+		self.u20=np.exp(1j*(self.px*self.xhalf+self.py*self.yhalf))
+		self.v10=np.exp(1j*(self.px*self.xhalf+self.py*self.y))
+		self.v20=np.exp(1j*(self.px*self.x+self.py*self.yhalf))
+		
+		self.N_zero=self.x[1,np.abs(self.x[1,:])==np.min(np.abs(self.x[1,:]))]
+		
+		#logical arrays for boundary conditions
+		self.rDu1=self.Drive_mat[1::2,1::2]!=0.0
+		self.rDu2=self.Drive_mat[0::2,0::2]!=0.0
+		self.rDv1=self.Drive_mat[1::2,0::2]!=0.0
+		self.rDv2=self.Drive_mat[0::2,1::2]!=0.0
+				
+		self.rAu1=self.Absorb_mat[1::2,1::2]!=0.0
+		self.rAu2=self.Absorb_mat[0::2,0::2]!=0.0
+		self.rAv1=self.Absorb_mat[1::2,0::2]!=0.0
+		self.rAv2=self.Absorb_mat[0::2,1::2]!=0.0
+		
+		self.rNPu1=self.No_prop_mat[1::2,1::2]!=0.0
+		self.rNPu2=self.No_prop_mat[0::2,0::2]!=0.0
+		self.rNPv1=self.No_prop_mat[1::2,0::2]!=0.0
+		self.rNPv2=self.No_prop_mat[0::2,1::2]!=0.0
+		
+		#V1-V4 are electrostatic scalr potentials for the v1,v2,u1,and u2
+		#latics respectively
+		self.V1=np.zeros(self.x.shape)
+		self.V2=np.zeros(self.x.shape)
+		self.V3=np.zeros(self.x.shape)
+		self.V4=np.zeros(self.x.shape)
+		
+		#precomputes sum/difference of mass and potential to reduce computation overhead
+		self.mminusV1=self.m-self.V1
+		self.mminusV2=self.m-self.V2
+		self.mplusV3=self.m+self.V3
+		self.mplusV4=self.m+self.V4
+		
+	def set_p(self,p0,theta):
+		#sets the injection wave-vector properties, p is amplitude, theta is direction
+		self.p0=p0
+		self.theta=theta
+		self.px=self.p0*np.cos(self.theta)
+		self.py=self.p0*np.sin(self.theta)
+		self.phi_p=np.arctan2(self.py,self.px)
+		self.u10=np.exp(1j*(self.px*self.x+self.py*self.y))
+		self.u20=np.exp(1j*(self.px*self.xhalf+self.py*self.yhalf))
+		self.v10=np.exp(1j*(self.px*self.xhalf+self.py*self.y+self.phi_p))
+		self.v20=np.exp(1j*(self.px*self.x+self.py*self.yhalf+self.phi_p))
+		
+	def get_pos_mat(self):
+		#outputs the stiched X,Y coordinates; can be useful for defining boundary conditions
+		Xtot_out=np.zeros((self.x.shape[0]*2,self.x.shape[1]*2))
+		Ytot_out=np.zeros((self.x.shape[0]*2,self.x.shape[1]*2))
+		Xtot_out[::2,::2]=self.xhalf
+		Xtot_out[1::2,::2]=self.xhalf
+		Xtot_out[::2,1::2]=self.x
+		Xtot_out[1::2,1::2]=self.x
+		Ytot_out[::2,::2]=self.yhalf
+		Ytot_out[1::2,::2]=self.y
+		Ytot_out[::2,1::2]=self.yhalf
+		Ytot_out[1::2,1::2]=self.y
+		return Xtot_out, Ytot_out
+		
+	def set_No_prop_mat(self,No_prop_mat):
+		#imports No_prop_mat and creates logical matrices to define propagation regions	
+		#and their edges
+		self.No_prop_mat=No_prop_mat!=0.0
+		self.Nprop_up=~self.No_prop_mat&np.roll(self.No_prop_mat,-1,axis=0)
+		self.Nprop_down=~self.No_prop_mat&np.roll(self.No_prop_mat,1,axis=0)
+		self.Nprop_right=~self.No_prop_mat&np.roll(self.No_prop_mat,-1,axis=1)
+		self.Nprop_left=~self.No_prop_mat&np.roll(self.No_prop_mat,1,axis=1)
+		
+		
+		temp=np.zeros(self.No_prop_mat.shape,dtype=np.uint8)
+		for i in range(-1,2,2):
+			for j in range(-1,2,2):
+				temp+=~(np.roll(np.roll(self.No_prop_mat,i,axis=0),j,axis=1))
+		
+		#Border of the no-prop region
+		self.Nprop_edge=(self.No_prop_mat)*temp
+		
+		self.rNPu1=self.No_prop_mat[1::2,1::2]
+		self.rNPu2=self.No_prop_mat[0::2,0::2]
+		self.rNPv1=self.No_prop_mat[1::2,0::2]
+		self.rNPv2=self.No_prop_mat[0::2,1::2]
+		
+		self.ru1_edge=self.Nprop_edge[1::2,1::2]
+		self.ru2_edge=self.Nprop_edge[0::2,0::2]
+		self.rv1_edge=self.Nprop_edge[1::2,0::2]
+		self.rv2_edge=self.Nprop_edge[0::2,1::2]
+		
+		self.ru1_edge0=self.ru1_edge>0
+		self.ru2_edge0=self.ru2_edge>0
+		self.rv1_edge0=self.rv1_edge>0
+		self.rv2_edge0=self.rv2_edge>0
+		
+		#Encodes pixel-by-pixel the existence or absence of nearest neighbors
+		self.ru1_edge=np.zeros((self.rNPu1.shape[0],(self.rNPu1.shape[1]),4),dtype=np.bool)
+		self.ru1_edge[:,:,0]=self.Nprop_up[1::2,1::2]
+		self.ru1_edge[:,:,1]=self.Nprop_right[1::2,1::2]
+		self.ru1_edge[:,:,2]=self.Nprop_down[1::2,1::2]
+		self.ru1_edge[:,:,3]=self.Nprop_left[1::2,1::2]
+		self.ru1_edgeX=self.ru1_edge[:,:,1]|self.ru1_edge[:,:,3]
+		self.ru1_edgeY=self.ru1_edge[:,:,0]|self.ru1_edge[:,:,2]
+				
+		self.ru2_edge=np.zeros((self.rNPu2.shape[0],(self.rNPu2.shape[1]),4),dtype=np.bool)
+		self.ru2_edge[:,:,0]=self.Nprop_up[0::2,0::2]
+		self.ru2_edge[:,:,1]=self.Nprop_right[0::2,0::2]
+		self.ru2_edge[:,:,2]=self.Nprop_down[0::2,0::2]
+		self.ru2_edge[:,:,3]=self.Nprop_left[0::2,0::2]
+		self.ru2_edgeX=self.ru2_edge[:,:,1]|self.ru2_edge[:,:,3]
+		self.ru2_edgeY=self.ru2_edge[:,:,0]|self.ru2_edge[:,:,2]
+
+		self.rv1_edge=np.zeros((self.rNPv1.shape[0],(self.rNPv1.shape[1]),4),dtype=np.bool)
+		self.rv1_edge[:,:,0]=self.Nprop_up[1::2,0::2]
+		self.rv1_edge[:,:,1]=self.Nprop_right[1::2,0::2]
+		self.rv1_edge[:,:,2]=self.Nprop_down[1::2,0::2]
+		self.rv1_edge[:,:,3]=self.Nprop_left[1::2,0::2]		
+		self.rv1_edgeX=self.rv1_edge[:,:,1]|self.rv1_edge[:,:,3]
+		self.rv1_edgeY=self.rv1_edge[:,:,0]|self.rv1_edge[:,:,2]
+
+	
+		self.rv2_edge=np.zeros((self.rNPv2.shape[0],(self.rNPv2.shape[1]),4),dtype=np.bool)
+		self.rv2_edge[:,:,0]=self.Nprop_up[0::2,1::2]
+		self.rv2_edge[:,:,1]=self.Nprop_right[0::2,1::2]
+		self.rv2_edge[:,:,2]=self.Nprop_down[0::2,1::2]
+		self.rv2_edge[:,:,3]=self.Nprop_left[0::2,1::2]
+		self.rv2_edgeX=self.rv2_edge[:,:,1]|self.rv2_edge[:,:,3]
+		self.rv2_edgeY=self.rv2_edge[:,:,0]|self.rv2_edge[:,:,2]
+
+		
+		
+	def set_Absorb_mat(self,Absorb_mat):
+		self.Absorb_mat=Absorb_mat
+		self.rAu1=self.Absorb_mat[1::2,1::2]!=0.0
+		self.rAu2=self.Absorb_mat[0::2,0::2]!=0.0
+		self.rAv1=self.Absorb_mat[1::2,0::2]!=0.0
+		self.rAv2=self.Absorb_mat[0::2,1::2]!=0.0
+		
+	def set_Drive_mat(self,Drive_mat):
+		self.Drive_mat=Drive_mat
+		self.rDu1=self.Drive_mat[1::2,1::2]!=0.0
+		self.rDu2=self.Drive_mat[0::2,0::2]!=0.0
+		self.rDv1=self.Drive_mat[1::2,0::2]!=0.0
+		self.rDv2=self.Drive_mat[0::2,1::2]!=0.0
+		
+	def v_step(self):
+		#steps the v-lattice relative to a fixed u-lattice
+		
+		self.t=self.t+self.D_t/2*pi
+		
+		#introduce the drive wave to the u-lattice
+		self.u1[self.rDu1]+=self.Drive_coupling*self.u10[self.rDu1]*np.exp(-1j*self.p0*self.t)
+		self.u2[self.rDu2]+=self.Drive_coupling*self.u20[self.rDu2]*np.exp(-1j*self.p0*self.t)
+		
+		#Apply absorption
+		self.u1[self.rAu1]*=self.Abs_rate
+		self.u2[self.rAu2]*=self.Abs_rate
+		
+		#wave equation
+		self.out=(1/(2*1j+self.D_t*self.mminusV1))*((2*1j-self.D_t*self.mminusV1)*self.v1-(2*1j*self.D_t/self.D_x)*(self.u1-np.roll(self.u1,1,axis=1))+(2*self.D_t/self.D_y)*(np.roll(self.u2,-1,axis=0)-self.u2))		
+		self.out2=(1/(2*1j+self.D_t*self.mminusV2))*((2*1j-self.D_t*self.mminusV2)*self.v2-(2*1j*self.D_t/self.D_x)*(np.roll(self.u2,-1,axis=1)-self.u2)+(2*self.D_t/self.D_y)*(self.u1-np.roll(self.u1,1,axis=0)))
+		
+		self.v1=self.out
+		self.v2=self.out2
+	
+	def u_step(self):
+		#steps the u-lattice relative to a fixed v lattice
+		self.t=self.t+self.D_t/2*pi
+	
+		#Apply absorption
+		self.v1[self.rAv1]*=self.Abs_rate
+		self.v2[self.rAv2]*=self.Abs_rate
+
+		#wave equation
+		self.out=(~self.rNPu1)*(1/(2*1j-self.D_t*self.mplusV3))*((2*1j+self.D_t*self.mplusV3)*self.u1-(2*1j*self.D_t/self.D_x)*(np.roll(self.v1,-1,axis=1)-self.v1)-(2*self.D_t/self.D_y)*(np.roll(self.v2,-1,axis=0)-self.v2))
+		self.out2=(~self.rNPu2)*(1/(2*1j-self.D_t*self.mplusV4))*((2*1j+self.D_t*self.mplusV4)*self.u2-(2*1j*self.D_t/self.D_x)*(self.v2-np.roll(self.v2,1,axis=1))-(2*self.D_t/self.D_y)*(self.v1-np.roll(self.v1,1,axis=0)))
+		
+		self.u1=self.out
+		self.u2=self.out2
+		
+		
+	def time_step(self):
+		#Takes a full time step D_t in two half steps
+		self.v_step()
+		self.u_step()
